86f6a47cf0201d5935f93b55d431523e94b9aae8
Omg-A <67595078+Omg-A@users.noreply.github.com>
2021-08-21 19:40:33 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-function preload(){
+EyeX = 0;
+EyeY = 0;
 
+EarX = 0;
+EarY = 0;
+
+IsFilterSunglass = true;
+
+function preload(){
+    Img = loadImage("Img.png");
+    Img2 = loadImage("Img2.png");
 }
 
 function setup(){
@@ -19,19 +28,49 @@ function modelLoaded(){
 
 function gotPoses(results){
     if(results.length > 0){
-        console.log(results);
-        console.log("Results of Left Eye X : " + results[0].pose.leftEye.x);
-        console.log("Results of Left Eye Y : " + results[0].pose.leftEye.y);
+        if(IsFilterSunglass == true){
+            console.log(results);
+            EyeX = results[0].pose.rightEye.x - 20;
+            EyeY = results[0].pose.leftEye.y - 20;
+            console.log("Results of Left Eye X : " + results[0].pose.leftEye.x);
+            console.log("Results of Left Eye Y : " + results[0].pose.leftEye.y);
 
         console.log("Results of Right Eye X : " + results[0].pose.rightEye.x);
         console.log("Results of Right Eye Y : " + results[0].pose.rightEye.y);
+        }else if(IsFilterSunglass == false){
+            
+            console.log(results);
+            EarX = results[0].pose.rightEar.x - 30;
+            EarY = results[0].pose.leftEar.y - 100;
+            console.log("Results of Left Ear X : " + results[0].pose.leftEar.x);
+            console.log("Results of Left Ear Y : " + results[0].pose.leftEar.y);
+
+            console.log("Results of Right Ear X : " + results[0].pose.rightEar.x);
+            console.log("Results of Right Ear Y : " + results[0].pose.rightEar.y);
+        }
+        
     }
 }
 
 function draw(){
     image(video, 0, 0, 300, 300);
+    if (IsFilterSunglass == true){
+        image(Img, EyeX, EyeY, 85, 45);
+    }else if (IsFilterSunglass == false){
+        image(Img2, EarX, EarY, 150, 150);
+    }
 }
 
 function take_snapshot(){
-    save("YourSunGlassFilterImage.png");
+    save("YourFilterImage.png");
+}
+
+function SunGlass(){
+    IsFilterSunglass = true;
+    console.log("Sunglass"); 
+}
+
+function HeadPhone(){
+    IsFilterSunglass = false;
+    console.log("Headphone"); 
 }
